cab122480794db5f7fa2b07c75c7b0cc6236a809
jk0 <wjkjyp@gmail.com>
2017-10-14 15:35:13 +0800

    remove AliSLSMessageGenerator's export

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
 export * from './lib/IAliSLSMessageAdapter';
 export * from './lib/AliSLSAPIMessageAdapter';
 export * from './lib/AliSLSTEXTMessageAdapter';
-export * from './lib/AliSLSMessageGenerator';
